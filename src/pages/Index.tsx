2a60938177a9159b37b55f5d276582d6ba0ce610
scottymoll <46937004+scottymoll@users.noreply.github.com>
2025-10-23 09:18:14 -0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/Header";
+import Header from "@/components/Header"; 
 import Footer from "@/components/Footer";
 import FeatureCard from "@/components/FeatureCard";
 import StepCard from "@/components/StepCard";
